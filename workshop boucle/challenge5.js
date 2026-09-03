2c964f774aabb13803381a036b1a1c1824bef826
yassine aboulayt <yassinee@gmail.com>
2026-09-03 10:30:04 +0100

    add challenge 5

---
+++ workshop boucle/challenge5.js
@@ -0,0 +1,14 @@
+let prompt = require('prompt-sync')();
+
+let base = Number(prompt('Entrez la base : '));
+let exposant = Number(prompt('Entrez un exposant entier positif ou nul : '));
+let resultat = 1;
+
+if (Number.isInteger(exposant) && exposant >= 0) {
+  for (let i = 1; i <= exposant; i++) {
+    resultat *= base;
+  }
+  console.log(`${base}^${exposant} = ${resultat}`);
+} else {
+  console.log('Veuillez entrer un exposant entier positif ou nul.');
+}
